9f6ba4912fb5b34d12d094f646002d07e3bd739d
unknown <tinashechitamba@outlook.com>
2023-06-12 20:56:33 +0200

    Added spring boot project

---
+++ backend/spring-boot/src/main/java/com/christembassy/zimre/portal/PortalApplication.java
@@ -0,0 +1,13 @@
+package com.christembassy.zimre.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalApplication.class, args);
+	}
+
+}
