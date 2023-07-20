0fad312e9d5185343e2ad9aae19fce12194cb2f3
unknown <tinashechitamba@outlook.com>
2023-07-20 18:38:16 +0200

    refacotred controller advice

--- src/main/java/com/christembassy/zimre/portal/exception/ControllerAdvisor.java
+++ src/main/java/com/christembassy/zimre/portal/exception/ControllerAdvisor.java
@@ -3,6 +3,7 @@ package com.christembassy.zimre.portal.exception;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -10,30 +11,27 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
   @Override
-  protected ResponseEntity<Object> handleMethodArgumentNotValid(
-          MethodArgumentNotValidException ex, HttpHeaders headers,
-          HttpStatus status, WebRequest request) {
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+                                                                HttpStatus status, WebRequest request) {
 
     Map<String, Object> body = new LinkedHashMap<>();
     body.put("timestamp", Timestamp.valueOf(LocalDateTime.now()));
     body.put("status", status.value());
 
-    Map<String, String> errs = new HashMap<>();
-    ex.getBindingResult()
+    Map<String, String> errors = ex.getBindingResult()
             .getFieldErrors()
-            .forEach( obj ->
-                    errs.put(obj.getField(), obj.getDefaultMessage())
-            );
+            .stream()
+            .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
 
-    body.put("errors", errs);
+    body.put("errors", errors);
     return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
   }
 
